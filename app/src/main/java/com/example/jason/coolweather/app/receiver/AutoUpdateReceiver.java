e24d3e12a4b1b02649664ad101744644fc82ce39
Jason Jason <Jason Jason>
2016-04-09 22:37:00 +0800

    增加了说明文档（简述）

--- app/src/main/java/com/example/jason/coolweather/app/receiver/AutoUpdateReceiver.java
+++ app/src/main/java/com/example/jason/coolweather/app/receiver/AutoUpdateReceiver.java
@@ -1,8 +1,10 @@
-package com.example.jason.coolweather.app.service;
+package com.example.jason.coolweather.app.receiver;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
+import com.example.jason.coolweather.app.service.AutoUpdateService;
 
 /**
  * Created by Jason on 4/9/2016.
